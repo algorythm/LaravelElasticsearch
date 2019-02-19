854e96cf4b91f44ed766b5395d1edd517f524b26
algorythm <anders@wiberg.tech>
2019-02-19 15:08:31 +0100

    Add search endpoint for articles

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Repository\ArticlesRepository;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -18,5 +9,13 @@
 Route::get('/', function() {
     return view('articles.index', [
         'articles' => App\Article::all(),
+    ]);
+});
+
+Route::get('/search', function(ArticlesRepository $repository) {
+    $articles = $repository->search((string) request('q'));
+
+    return view('articles.index', [
+        'articles' => $articles,
     ]);
 });
