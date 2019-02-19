a7a91d6a8a28606b51f731e4490cf9aacac52a48
algorythm <anders@wiberg.tech>
2019-02-19 15:24:02 +0100

    Better tags

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Articles</title>
-    {{-- <link rel="stylesheet" href="{{ elixir('css/app.css') }}" /> --}}
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
@@ -32,7 +31,11 @@
                             <div class="card-body">
                                 <h5 class="card-title">{{ $article->title }}</h5>
                                 <p class="card-text">{{ $article->body }}</p>
-                                <span class="badge badge-secondary">{{ implode(', ', $article->tags ?:[]) }}</span>
+                                @forelse ($article->tags as $tag)
+                                    <span class="badge badge-secondary">{{ $tag }}</span>
+                                @empty
+                                    <small class="text-muted">No tags</small>
+                                @endforelse
                             </div>
                         </div>
                     @empty
